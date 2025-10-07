81b384ca38cb8444c99ce379e274451aec4ab475
JaydenRRamirez <156058380+JaydenRRamirez@users.noreply.github.com>
2025-10-07 02:44:06 +0000

    Added Button, Asset may change later(Step 1 Complete)

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
+/*
 import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
 document.body.innerHTML = `
   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
 `;
+*/
+import "./style.css";
+import emoji from "./tumblr_c016e4951490e803e0522d9d6ca08c35_3f750c1e_1280.png";
+
+const button = document.createElement("button");
+
+button.innerHTML =
+  `<img src="${emoji}" alt="Fun Emoji" style="width: 240px; height: 240px;">`;
+document.body.append(button);
