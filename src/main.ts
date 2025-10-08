5be1dab206809c74d3a48e88ba687ee0e3e9d220
JaydenRRamirez <156058380+JaydenRRamirez@users.noreply.github.com>
2025-10-08 04:46:30 +0000

    Added a Counter and Click functionality, as well as display (Step 2 Complete)

--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,26 @@ document.body.innerHTML = `
 import "./style.css";
 import emoji from "./tumblr_c016e4951490e803e0522d9d6ca08c35_3f750c1e_1280.png";
 
+let counter: number = 0;
+const funUnit = "Hi Five to Angel Gabby";
+
+const counterUI = document.createElement("div");
+
+function UI() {
+  counterUI.innerHTML = `${counter} ${funUnit}`;
+};
+
+UI();
+
 const button = document.createElement("button");
 
 button.innerHTML =
   `<img src="${emoji}" alt="Fun Emoji" style="width: 240px; height: 240px;">`;
 document.body.append(button);
+
+document.body.append(counterUI, button);
+
+button.addEventListener("click", () => {
+  counter += 1;
+  UI();
+});
