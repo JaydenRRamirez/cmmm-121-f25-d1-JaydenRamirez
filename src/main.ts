3ca0dbbdacd7e7646b091aa4be7c4e8c4f86a186
JaydenRRamirez <156058380+JaydenRRamirez@users.noreply.github.com>
2025-10-10 06:25:21 +0000

    Added in an Upgrade System and It's button with a corresponding Emoji, Continuous Growth will now correspond with the Upgrade rather than instantly starting (Step 5 Complete)

--- src/main.ts
+++ src/main.ts
@@ -11,14 +11,34 @@ Diego was also here
 */
 import "./style.css";
 import emoji from "./tumblr_c016e4951490e803e0522d9d6ca08c35_3f750c1e_1280.png";
+import upgradeEmoji from "./Francis.png";
 
 let counter: number = 0;
-const funUnit = "Hi Five to Angel Gabby";
+let growthRate: number = 0;
+const funUnit = "Hi-Five to Angel Gabby";
+const cost: number = 10;
 
 const counterUI = document.createElement("div");
+const rateUI = document.createElement("div");
+const upgradeUI = document.createElement("div");
+const upgradeButton = document.createElement("button");
 
 function UI() {
   counterUI.innerHTML = `${counter.toFixed(0)} ${funUnit}`;
+
+  rateUI.innerHTML = `${growthRate.toFixed(0)} Francis helping.`;
+
+  upgradeUI.innerHTML = `Purchase a Friend Francis for ${cost}`;
+
+  if (counter >= cost) {
+    upgradeButton.disabled = false;
+
+    upgradeButton.title = "Click to Purchase!";
+  } else {
+    upgradeButton.disabled = true;
+
+    upgradeButton.title = `Need ${cost.toFixed(0)} Hi-Fives to purchase.`;
+  }
 }
 
 UI();
@@ -35,13 +55,31 @@ button.addEventListener("click", () => {
   counter += 1;
   UI();
 });
+
+upgradeButton.innerHTML =
+  `<img src="${upgradeEmoji}" alt="Upgrade Francis" style="width: 60px; height: 60px;"><br>PURCHASE`;
+
+upgradeButton.addEventListener("click", () => {
+  if (counter >= cost) {
+    counter -= cost;
+    growthRate += 1;
+  }
+});
+
+document.body.append(counterUI, rateUI, document.createElement("hr")); // Separator
+document.body.append(
+  button,
+  document.createElement("br"),
+  document.createElement("br"),
+);
+document.body.append(upgradeUI, upgradeButton);
 /*
 setInterval(() => {
   counter += 1;
   UI();
 }, 1000);
 */
-
+/*
 let timestamp: number = 0;
 const frames = 1 / 1000;
 
@@ -55,6 +93,25 @@ function ContinuousGrowth() {
   const unitRate = current - timestamp;
 
   const growth = unitRate * frames;
+
+  counter += growth;
+
+  UI();
+
+  timestamp = current;
+
+  requestAnimationFrame(ContinuousGrowth);
+}
+
+requestAnimationFrame(ContinuousGrowth);
+*/
+
+let timestamp: number = performance.now();
+
+function ContinuousGrowth(current: number) {
+  const unitRate = (current - timestamp) / 1000;
+
+  const growth = growthRate * unitRate;
 
   counter += growth;
 
